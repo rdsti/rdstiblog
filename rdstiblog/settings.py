6f9216589b5e2fb75f9bd8e98261822adbceddf5
Rodrigo Donizete Siqueira <rodrigo.siqueira@rdsti.com.br>
2021-06-23 00:00:17 -0300

    Correção erro deploy diretório temp

--- rdstiblog/settings.py
+++ rdstiblog/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['static']
 
 # Configure Django App for Heroku.
 import django_heroku
